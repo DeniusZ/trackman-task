d38afec967a9d0e562505d1483ca9ba73715df3f
Deniss Zubkovs <deniss.zubkovs@issuu.com>
2025-05-20 10:37:00 +0200

    chore(app): add go back button to PageNotFound. add trackman favicon

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Button } from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const StyledPageNotFound = styled.main`
   height: 100vh;
@@ -11,23 +13,27 @@ const StyledPageNotFound = styled.main`
 
 const Box = styled.div`
   background-color: var(--color-white);
-  border: 1px solid var(--color-gray-700);
+  border: 1px solid var(--color-gray-300);
   border-radius: var(--radius-xl);
 
   padding: 4.8rem;
-  flex: 0 1 96rem;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  & h1 {
+  > h1 {
     margin-bottom: 3.2rem;
   }
 `;
 
 export const PageNotFound: React.FC = () => {
+  const navigate = useNavigate();
+  const moveBack = () => navigate(-1);
   return (
     <StyledPageNotFound>
       <Box>
         <h1>The page you are looking for could not be found 😢</h1>
+        <Button onClick={moveBack}>Go back</Button>
       </Box>
     </StyledPageNotFound>
   );
